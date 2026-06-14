f7aba80a4bad17db0f890f48aea2935bfba8eebf
Brooklyn Ochoa Bio <BkLynn@disroot.org>
2026-06-13 22:19:09 -0700

    v 1.0.3

--- js/include.js
+++ js/include.js
@@ -1,5 +1,10 @@
 (async function() {
   try {
+    const pathParts = location.pathname.replace(/\/$/, '').split('/').filter(Boolean);
+    const isProjectSite = location.hostname === 'brooklyn-git.github.io';
+    const depth = isProjectSite ? Math.max(0, pathParts.length - 1) : pathParts.length;
+    const fetchPrefix = depth > 0 ? '../'.repeat(depth) : '';
+
     async function tryFetch(paths) {
       for (const p of paths) {
         try {
@@ -11,12 +16,19 @@
     }
 
     const [headerHtml, footerHtml] = await Promise.all([
-      tryFetch(['header.html', '../header.html']),
-      tryFetch(['footer.html', '../footer.html']),
+      tryFetch([fetchPrefix + 'header.html', '../header.html', 'header.html']),
+      tryFetch([fetchPrefix + 'footer.html', '../footer.html', 'footer.html']),
     ]);
     const headerEl = document.getElementById('header-placeholder');
     const footerEl = document.getElementById('footer-placeholder');
-    if (headerEl) headerEl.innerHTML = headerHtml;
+    if (headerEl) {
+      headerEl.innerHTML = headerHtml;
+      if (depth > 0) {
+        headerEl.querySelectorAll('a[href]:not([href*="://"]):not([href^="#"])').forEach(a => {
+          a.href = '../' + a.getAttribute('href');
+        });
+      }
+    }
     if (footerEl) footerEl.innerHTML = footerHtml;
     const saved = localStorage.getItem('bronx-lang');
     setLang(saved === 'en' || saved === 'es' ? saved : 'es');
